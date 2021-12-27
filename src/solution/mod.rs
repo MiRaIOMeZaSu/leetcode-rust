17399e4cc02bb4030171e9247a09cc4973445e07
MiRaIOMeZaSu <379297266@qq.com>
2021-12-27 23:27:06 +0800

    825: wrong answer

--- src/solution/mod.rs
+++ src/solution/mod.rs
@@ -2,3 +2,4 @@ mod s67_add_binary;
 mod s1154_day_of_year;
 mod s686_repeated_string_match;
 mod s264_nth_ugly_number;
+mod s825_num_friend_requests;
---
+++ src/solution/s825_num_friend_requests.rs
@@ -0,0 +1,64 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn num_friend_requests(ages: Vec<i32>) -> i32 {
+        // 使用滑动窗口法
+        let mut agesMut: Vec<i32> = vec![];
+        for age in ages {
+            agesMut.push(age);
+        }
+        agesMut.sort();
+        let size: usize = agesMut.len();
+        let mut left: i32 = -1;
+        // let mut right: i32 = -1;
+        let mut ans: i32 = 0;
+        let mut i: usize = 0;
+        while i < size {
+            let mut index: usize = usize::from((left + 1) as u16);
+            while index < i {
+                if agesMut[index] * 2 <= agesMut[i] + 14 {
+                    // 不可以发送分支
+                    left += 1;
+                    index += 1;
+                    // left+1不能发送
+                } else {
+                    // 可以发送分支
+                    // left+1能够发送,但left不能发送
+                    break;
+                }
+            }
+            if index < i {
+                ans += i as i32 - (left + 1);
+            }
+            i += 1;
+        }
+        // 检查向后走相同的
+        i = 0;
+        let mut curr: i32 = 1;
+        while i < size {
+            if i + 1 < size && agesMut[i + 1] == agesMut[i] {
+                curr += 1;
+            } else {
+                // 断掉了
+                // 总长为curr
+                if curr > 1 {
+                    ans += (curr * (curr - 1)) / 2;
+                }
+                curr = 1;
+            }
+            i += 1;
+        }
+        return ans;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_825() {
+        assert_eq!(2, Solution::num_friend_requests(vec![16, 16]));
+        assert_eq!(2, Solution::num_friend_requests(vec![16, 17, 18]));
+    }
+}
