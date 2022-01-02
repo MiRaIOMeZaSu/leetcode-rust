1ca74739df41acb749ccf73dd30d128094924939
MiRaIOMeZaSu <379297266@qq.com>
2022-01-02 23:41:19 +0800

    390: done

--- src/solution/s390_last_remaining.rs
+++ src/solution/s390_last_remaining.rs
@@ -6,39 +6,28 @@ fn is_even(n: i32) -> bool {
 
 impl Solution {
     pub fn last_remaining(n: i32) -> i32 {
-        let mut mutiple: i32 = 1;
-        let mut divide: i32 = 1;
-        let mut left: i32 = n;
+        let mut left: i32 = 1;
+        let mut gap: i32 = 1;
+        let mut size: i32 = n;
         if n == 1 {
             return 1;
         }
-        let mut toRightNow: bool = true;
-        while left >= 4 {
+        // 直接每次都计算起始点,数量和间隔
+        while size > 1 {
             // 往右
-            mutiple *= 2;
-            left /= 2;
+            left += gap;
+            gap *= 2;
+            size /= 2;
             // 往左
-            toRightNow = false;
-            if left >= 4 {
-                if is_even(left) {
-                    // 移除的是偶数位
-                    divide *= 2;
-                } else {
-                    mutiple *= 2;
+            if size > 1 {
+                if !is_even(size) {
+                    left += gap;
                 }
-                left /= 2;
-                toRightNow = true;
+                gap *= 2;
+                size/=2;
             }
         }
-        let mut ans: i32 = mutiple;
-        if left == 3 {
-            ans += divide * mutiple;
-        } else if left == 2 {
-            if toRightNow {
-                ans += divide * mutiple;
-            }
-        }
-        return ans;
+        return left;
     }
 }
 
